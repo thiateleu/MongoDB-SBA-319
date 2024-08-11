dd6cef67ce178ed16a4be39b9c56579d381b4845
thiateleu <thiateleu@gmail.com>
2024-08-10 21:30:02 -0400

    first commit

---
+++ db/conn.mjs
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Global congiguration
+const mongoURI = process.env.MONGO_URI;
+const db = mongoose.connection;
+
+// Connect to Mongo
+mongoose.connect(mongoURI);
+mongoose.connection.once("open", () => {
+  console.log("connected  to mongoDB");
+});
+
+export default db;
